51473e278709e3589126e302a3493a8c7373bf43
Arthur Abrantes <abrantesarthur1997@gmail.com>
2025-12-05 13:34:50 -0300

    update README

--- pulumi/image/commands/installCertificate/constants.ts
+++ pulumi/image/commands/installCertificate/constants.ts
@@ -1,4 +1,3 @@
-// FIXME: read this from configuration
 export const REMOTE_KEY_DIR = "/etc/ssl/private";
 export const REMOTE_KEY_PATH = `${REMOTE_KEY_DIR}/cloudflare-origin.key`;
 export const REMOTE_CERT_DIR = "/etc/ssl/certs";
